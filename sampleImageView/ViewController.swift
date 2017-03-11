e38fefeeeb10b82583682e24c7526705d758ecdd
ei17ringo <ei17ringo@gmail.com>
2017-03-11 12:03:58 +0800

    commit init

--- sampleImageView/ViewController.swift
+++ sampleImageView/ViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //画像１ボタンが押されたとき
+    @IBAction func tapImage1(_ sender: UIButton) {
+        myImageView.image = UIImage(named: "sample.png")
+    }
+    
+    //画像２ボタンが押されたとき
+    @IBAction func tapImage2(_ sender: UIButton) {
+        myImageView.image = UIImage(named: "sample2.png")
+    }
+  
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
